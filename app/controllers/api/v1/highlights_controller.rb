c888c71a14f64f3a844177df0504e4efcccb0752
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-10-23 15:40:13 -0400

    updated requests to fetch 6 most recent pages of highlights, comments added for readability

--- app/controllers/api/v1/highlights_controller.rb
+++ app/controllers/api/v1/highlights_controller.rb
@@ -1,13 +1,30 @@
 class Api::V1::HighlightsController < ApplicationController
 
   def get_highlights
+      #initial request to reddit/r/nba
       reddit_posts_response = RestClient.get('http://www.reddit.com/r/nba.json')
       parsed_posts = JSON.parse(reddit_posts_response)
 
+      # get the value of the after: in api response to get additional pages of of highlights
       next_page_id = parsed_posts["data"]["after"]
 
+      # filter api response so that only video highlights are persisted
       highlights = parsed_posts["data"]["children"].select {|post| post["data"]["link_flair_css_class"] == "highlights" && !post["data"]["media_embed"]["content"].nil?}
 
+      # get highlights from the five pages after r/nba's frontpage
+      5.times do
+        reddit_posts_response = RestClient.get('http://www.reddit.com/r/nba.json?after=' + next_page_id)
+        parsed_posts = JSON.parse(reddit_posts_response)
+
+        next_page_id = parsed_posts["data"]["after"]
+
+        highlights.push(parsed_posts["data"]["children"].select {|post| post["data"]["link_flair_css_class"] == "highlights" && !post["data"]["media_embed"]["content"].nil?})
+
+        highlights.flatten!
+      end
+
+
+      # persist highlights, duplicates will be rejected because of unique title validation
       highlights.each do |highlight|
         Highlight.create(
           title: highlight["data"]["title"],
@@ -21,6 +38,7 @@ class Api::V1::HighlightsController < ApplicationController
       # order persisted highlghts by the date they were created on reddit
       highlights_to_send = Highlight.all.order(date_created: :desc)
 
+      #send all highlights to the frontend
       render json: {highlights: highlights_to_send}
   end
 
