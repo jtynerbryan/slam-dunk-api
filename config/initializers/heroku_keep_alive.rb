00047f975e0e864cc4410800e66ca424ff8f99b2
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-19 13:06:51 -0400

    removed rufus scheduler for now

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -1,8 +1,8 @@
-require 'rufus/scheduler'
-scheduler = Rufus::Scheduler new
-
-scheduler.every '10m' do
-   require "net/http"
-   require "uri"
-   Net::HTTP.get_response(URI.parse(ENV["API_PING"]))
-end
+# require 'rufus/scheduler'
+# scheduler = Rufus::Scheduler new
+#
+# scheduler.every '10m' do
+#    require "net/http"
+#    require "uri"
+#    Net::HTTP.get_response(URI.parse(ENV["API_PING"]))
+# end
