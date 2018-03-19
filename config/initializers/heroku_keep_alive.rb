8b2b43b8e4fd85711ae81ea42d47b391567ffbdd
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-19 13:04:08 -0400

    trying rufus outside production environment

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -1,11 +1,8 @@
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler new
 
-if Rails.env.production?
-  scheduler.every '10m' do
-     # require "net/http"
-     # require "uri"
-     # Net::HTTP.get_response(URI.parse(ENV["API_PING"]))
-     puts 'test'
-  end
+scheduler.every '10m' do
+   require "net/http"
+   require "uri"
+   Net::HTTP.get_response(URI.parse(ENV["API_PING"]))
 end
