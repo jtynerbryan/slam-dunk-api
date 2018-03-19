a98140e022d27534323255cc6f7db7cc59d3a612
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-19 12:41:19 -0400

    keep heroku alive

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -1,10 +1,10 @@
-# require 'rufus/scheduler'
-# scheduler = Rufus::Scheduler new
-#
-# if Rails.env.production?
-#   scheduler.every '10m' do
-#      require "net/http"
-#      require "uri"
-#      Net::HTTP.get_response(URI.parse(ENV["WEB_PING"]))
-#   end
-# end
+require 'rufus/scheduler'
+scheduler = Rufus::Scheduler new
+
+if Rails.env.production?
+  scheduler.every '10m' do
+     require "net/http"
+     require "uri"
+     Net::HTTP.get_response(URI.parse(ENV["API_PING"]))
+  end
+end
